71067c9d3cc32787329b90491901ea63751bc7b1
Buğra Altıntaş <bugraltintas@gmail.com>
2022-05-19 19:45:51 +0300

    PART-I & PART-II completed

--- hw2.cpp
+++ hw2.cpp
@@ -35,10 +35,13 @@ int n,m; //  grid dimensions
 pthread_mutex_t** gridMutex; // mutexes for each cell in grid
 pthread_mutex_t checkMutex;
 sem_t** gridSem; // semaphore for each cell
+pthread_cond_t*** cvAvailable;
+pthread_mutex_t availableLock;
 
 pthread_mutex_t breakLock;
 pthread_cond_t cvBreak = PTHREAD_COND_INITIALIZER;
 int Break;
+int Stop;
 
 pthread_mutex_t contLock;
 pthread_cond_t cvCont = PTHREAD_COND_INITIALIZER;
@@ -46,6 +49,7 @@ pthread_cond_t cvCont = PTHREAD_COND_INITIALIZER;
 sem_t sleepReq;
 sem_t awake;
 
+pthread_mutex_t dummyLock;
 
 ///////// FUNCTIONS
 /*
@@ -85,142 +89,118 @@ void printGrid(){
     }
 }
 
-void waitCells(pair<int,int>& coord, int si, int sj){ 
+int waitCells(pair<int,int>& coord, int si, int sj, int gid){ 
     int boundary_i = coord.first+si < n ? coord.first+si : n ;
     int boundary_j = coord.second+sj < m ? coord.second+sj : m;
+    bool checkAgain = true;
 
-    bool entryFlag = true;
-    int criticalCell_i,criticalCell_j;
-    /*
-    pthread_mutex_lock(&checkMutex);
-    for(int i=coord.first;i<boundary_i;i++){
-        for(int j= coord.second;j<boundary_j;j++){
-            if(numOfGatherers[i][j] + 1 != 1){
-                entryFlag = false;
-                criticalCell_i = i;
-                criticalCell_j = j;
+    // check cvAvailable: checking if all cells are free
+    while(checkAgain){
+        checkAgain = false;
+        pthread_mutex_lock(&availableLock);
+        for(int i=coord.first;i<boundary_i;i++){
+            for(int j=coord.second;j<boundary_j;j++){
+                //if(gid == 1) cerr << "G1 is checking " << i << " - " << j << ": " << (cvAvailable[i][j] == NULL) << endl;
+                if(cvAvailable[i][j] == NULL)
+                    continue;
+                //cerr << "G" << gid << " waiting on " << i << " - " << j << endl;
+                pthread_cond_wait(cvAvailable[i][j],&availableLock);// unlocks the lock before blocking, locks after returning
+                
+                // stop and break orders should be added here
+                pthread_mutex_lock(&breakLock);
+                if(Stop){
+                    pthread_mutex_unlock(&breakLock);
+                    pthread_mutex_unlock(&availableLock);
+                    hw2_notify(GATHERER_STOPPED,gid,0,0);
+                    return 2;
+                }
+                else if(Break){
+                    pthread_mutex_unlock(&breakLock);
+                    pthread_mutex_unlock(&availableLock);
+                    hw2_notify(GATHERER_TOOK_BREAK,gid,0,0);
+                    pthread_mutex_lock(&contLock);
+                    pthread_cond_wait(&cvCont,&contLock);
+                    pthread_mutex_unlock(&contLock);
+                    hw2_notify(GATHERER_CONTINUED, gid, 0 ,0);
+                    return 1;
+                }
+                else
+                    pthread_mutex_unlock(&breakLock);
+                checkAgain = true;
+                break;
             }
         }
+        pthread_mutex_unlock(&availableLock);
     }
-    if(entryFlag){// we can lock all cells
-        for(int i=coord.first;i<boundary_i;i++){
-            for(int j= coord.second;j<boundary_j;j++){
-                wait(&gridSem[i][j]);
-                numOfGatherers[i][j]++;
-            }
-        }
-        pthread_mutex_unlock(&checkMutex);
-    }
-    else{// wait on critical cell, then wait on all cells
-        pthread_mutex_unlock(&checkMutex);
-        wait(&gridSem[criticalCell_i][criticalCell_j]);
-        // we may need one more check here
-        for(int i=coord.first;i<boundary_i;i++){
-            for(int j= coord.second;j<boundary_j;j++){
-                if(i!= criticalCell_i && j!=criticalCell_j)
-                    wait(&gridSem[i][j]);
-                numOfGatherers[i][j]++;
-            }
-        }        
-    }
-    */
 
+    //cerr << "G"<< gid << "is gonna lock the area" << endl; 
     
     for(int i=coord.first;i<boundary_i;i++){
         for(int j=coord.second;j<boundary_j;j++){
-            //cerr << "waitinging on cell " << i << "-" << j  << " r: ";
-            //cerr << wait(&gridCond[i][j], &gridMutex[i][j]) << endl;
             wait(&gridSem[i][j]);
-            /*pthread_mutex_lock(&breakLock);
-            if(Break){
-                hw2_notify(GATHERER_TOOK_BREAK,0,0,0);
-                //unlock the area that locked to now
-            }
-            pthread_mutex_unlock(&breakLock);*/
-            /*int fd_out = open("out", O_WRONLY | O_APPEND | O_CREAT, 0777);
-            dup2(fd_out,1);
-            cout << "WAITING ON (" << i << "," << j << ")" <<  endl; 
-            cout << "PROCEEDING ON (" << i << "," << j << ")" <<  endl; */
+            pthread_mutex_lock(&availableLock);
+            cvAvailable[i][j] = new pthread_cond_t;
+            pthread_cond_init(cvAvailable[i][j],NULL);
+            pthread_mutex_unlock(&availableLock);
         }
     }
+    return 0;
 }
 
-// lock a dedicated grid area to gather cigbutts.
-void lockCells(pair<int,int>& coord, int si, int sj){ 
-    int boundary_i = coord.first+si < n ? coord.first+si : n ;
-    int boundary_j = coord.second+sj < m ? coord.second+sj : m;
-    for(int i=coord.first;i<boundary_i;i++){
-        for(int j=coord.second;j<boundary_j;j++){
-            pthread_mutex_lock(&gridMutex[i][j]);
-        }
-    }
-}
 
-// unlock the area to let others to run.
-void unlockCells(pair<int,int>& coord, int si, int sj){
-    int boundary_i = coord.first+si < n ? coord.first+si : n ;
-    int boundary_j = coord.second+sj < m ? coord.second+sj : m;
-    for(int i=coord.first;i<boundary_i;i++){
-        for(int j=coord.second;j<boundary_j;j++){
-            pthread_mutex_unlock(&gridMutex[i][j]);
-        }
-    }
-}
 
-void signalCells(pair<int,int>& coord, int si, int sj){ 
+
+void signalCells(pair<int,int>& coord, int si, int sj,int gid){ 
     int boundary_i = coord.first+si < n ? coord.first+si : n ;
     int boundary_j = coord.second+sj < m ? coord.second+sj : m;
+    pthread_mutex_lock(&availableLock);
     for(int i=coord.first;i<boundary_i;i++){
         for(int j=coord.second;j<boundary_j;j++){
-            //signal(&gridCond[i][j]);
-            /*pthread_mutex_lock(&checkMutex);
-            numOfGatherers[i][j]--;
-            pthread_mutex_unlock(&checkMutex);*/
+            pthread_cond_broadcast(cvAvailable[i][j]);
+            //cerr << "G" << gid << " signaled " << i << "x" << j << endl;
             signal(&gridSem[i][j]);
+            pthread_cond_destroy(cvAvailable[i][j]);
+            cvAvailable[i][j] = NULL;
+            
         }
     }
+    pthread_mutex_unlock(&availableLock);
 }
 
 int cleanArea(pair<int,int>& coord, int si, int sj, int tg, int gid){
-    int i,j;
     int boundary_i = coord.first+si < n ? coord.first+si : n ;
     int boundary_j = coord.second+sj < m ? coord.second+sj : m;
-    int localBreak = 0;
     int tw;
-    //usleep(1000*tg);
-    for(i=coord.first;i<boundary_i;i++){
-        for(j=coord.second;j<boundary_j;j++){
+    for(int i=coord.first;i<boundary_i;i++){
+        for(int j=coord.second;j<boundary_j;j++){
             while(grid[i][j] > 0){
                 struct timeval current_time;// sleeping time calculations
                 struct timespec waiting_time;
                 gettimeofday(&current_time, NULL);
-                waiting_time.tv_sec = current_time.tv_sec;
-                long long nsec = current_time.tv_usec*1000;
-                nsec += tg*1000000;
-                waiting_time.tv_sec += nsec / 1000000000L;  
-                waiting_time.tv_nsec = nsec % 1000000000L;
-                //cerr << "G" << gid << " is about the get the break lock" << endl;
+                current_time.tv_usec += tg*1000;
+                waiting_time.tv_sec = current_time.tv_sec + current_time.tv_usec/1000000;
+                waiting_time.tv_nsec = (current_time.tv_usec%1000000)*1000;
+
                 pthread_mutex_lock(&breakLock);
-                //cerr << "G" << gid << " has the break lock" << endl;
-                while(!Break){// NOT SURE IF THIS IS NECESSARY
-                    tw = pthread_cond_timedwait(&cvBreak,&breakLock,&waiting_time);
-                    if(tw == ETIMEDOUT){// not on a break  
-                        break;  
-                    }
-                    else{
-                        pthread_mutex_unlock(&breakLock);
-                        signalCells(coord,si,sj); //unlock all cells
-                        hw2_notify(GATHERER_TOOK_BREAK, gid, 0, 0);
-                        pthread_mutex_lock(&contLock);
-                        pthread_cond_wait(&cvCont,&contLock);
-                        pthread_mutex_unlock(&contLock);
-                        hw2_notify(GATHERER_CONTINUED, gid, 0 ,0);
-                        return 1;
-                    }
+                tw = pthread_cond_timedwait(&cvBreak,&breakLock,&waiting_time); // it releases the breakLock before blocking, acquires it before returning
+                if(tw == ETIMEDOUT)// not on a break  
+                    pthread_mutex_unlock(&breakLock);
+                else if(Stop){// end the thread
+                    pthread_mutex_unlock(&breakLock);
+                    signalCells(coord,si,sj,gid); //unlock all cells
+                    hw2_notify(GATHERER_STOPPED,gid,0,0);
+                    return 2;
                 }
-                pthread_mutex_unlock(&breakLock);
-                // this awake and sleepReq method is working true for single thread, look for multiple
-                //usleep(1000*tg);
+                else if(Break){// take a break
+                    pthread_mutex_unlock(&breakLock);
+                    signalCells(coord,si,sj,gid); //unlock all cells
+                    hw2_notify(GATHERER_TOOK_BREAK, gid, 0, 0);
+                    pthread_mutex_lock(&contLock);
+                    pthread_cond_wait(&cvCont,&contLock);
+                    pthread_mutex_unlock(&contLock);
+                    hw2_notify(GATHERER_CONTINUED, gid, 0 ,0);
+                    return 1;
+                }
                 grid[i][j]--;
                 hw2_notify(GATHERER_GATHERED, gid, i, j);
             }
@@ -229,22 +209,6 @@ int cleanArea(pair<int,int>& coord, int si, int sj, int tg, int gid){
     hw2_notify(GATHERER_CLEARED, gid, 0, 0);
     return 0;
 }
-/*
-cerr << "xxxxxxxxxxxxxxxx" << endl;
-                pthread_cond_timedwait(&cvBreak,&breakLock,&waiting_time);
-                if(Break){
-                    cerr << "BBBBBBBBBBBBBBBBBBB" <<endl;
-                    pthread_mutex_unlock(&breakLock);
-                    hw2_notify(GATHERER_TOOK_BREAK, gid, 0, 0);
-                    // wait on continueCond
-                    pthread_mutex_lock(&contLock);
-                    pthread_cond_wait(&cvCont,&contLock);
-                    pthread_mutex_unlock(&contLock);
-                    hw2_notify(GATHERER_CONTINUED, gid, 0, 0);
-                }
-                else
-                    pthread_mutex_unlock(&breakLock);
-*/
 void executeOrders(vector<pair<int,string>>& orders, pthread_t* tids){
     int time = 0;
     int t;
@@ -254,24 +218,28 @@ void executeOrders(vector<pair<int,string>>& orders, pthread_t* tids){
         usleep(t*1000);
         time = o.first;
         if(!o.second.compare("break")){ // send break signal to all gatherers
+            hw2_notify(ORDER_BREAK,0,0,0);
             pthread_mutex_lock(&breakLock);
             Break = 1;
             pthread_mutex_unlock(&breakLock);
             pthread_cond_broadcast(&cvBreak);
-            hw2_notify(ORDER_BREAK,0,0,0);
         }
         else if(!o.second.compare("continue")){ // send continue signal to all gatherers
+            hw2_notify(ORDER_CONTINUE,0,0,0);
             pthread_mutex_lock(&breakLock);
             if(Break){
                 pthread_cond_broadcast(&cvCont);
             }
             Break = 0;
             pthread_mutex_unlock(&breakLock);
-            hw2_notify(ORDER_CONTINUE,0,0,0);
         }
         else if(!o.second.compare("stop")){ // send stop signal to all gatherers
-
             hw2_notify(ORDER_STOP,0,0,0);
+            pthread_mutex_lock(&breakLock);
+            Break = 0;
+            Stop = 1;
+            pthread_mutex_unlock(&breakLock);
+            pthread_cond_broadcast(&cvBreak);
         }
         else
             cerr << "INVALID ORDER" << endl;
@@ -287,32 +255,51 @@ void *gatherer(void *arg){ //arguments: grid, private
     int sj = p->sj;
     int tg = p->tg;
     int restartReq = 1;
+    int backupCoord=0;
+    int size = p->areas.size();
+    int c = 0;
     while(restartReq){
-        restartReq = 0;
-        for(auto coord : p->areas){ 
-            //  is semaphores and locks doing same job? probably yes but inspect it.
-            //  nevertheless, it's working right.
-            
+        restartReq = 0; // should be unnecessary
+        c++;
+        for(int coord=backupCoord;coord<size;coord++ ){ 
+                pthread_mutex_lock(&dummyLock);
+                //cerr << "G" << p->gid << " is waiting " <<  c << endl;
+                pthread_mutex_unlock(&dummyLock);
             // WAIT FOR ALL CELLS IN THE AREA
-            waitCells(coord, si, sj);
+            restartReq = waitCells(p->areas[coord], si, sj,p->gid);
+                pthread_mutex_lock(&dummyLock);
+                //cerr << "G" << p->gid << " is finished waiting " <<  c << endl;
+                pthread_mutex_unlock(&dummyLock);
+            if(restartReq==1){
+                backupCoord =coord;
+                break;
+            }
+            else if(restartReq==2){
+                pthread_exit(NULL);
+            }
 
-            // LOCK THE AREA
-            //lockCells(coord, si, sj);
-
-            hw2_notify(GATHERER_ARRIVED, p->gid, coord.first, coord.second);
+            hw2_notify(GATHERER_ARRIVED, p->gid, p->areas[coord].first, p->areas[coord].second);
             
             // CLEAN AREA
-            restartReq = cleanArea(coord, si, sj, tg, p->gid);
-            if(restartReq) break;
-            // UNLOCK THE AREA
-            //unlockCells(coord, si ,sj);
+            restartReq = cleanArea(p->areas[coord], si, sj, tg, p->gid);
+            if(restartReq==1){
+                                if(p->gid == 1 || p->gid==3)
+                    usleep(300000);
+                //pthread_mutex_lock(&dummyLock);
+                //cerr << "G" << p->gid << " will be restarted " << c<< endl;
+                //pthread_mutex_unlock(&dummyLock);                
+                backupCoord =coord;
+                break;
+            }
+            else if(restartReq==2){
+                pthread_exit(NULL);
+            }
 
             // SIGNAL FOR ALL CELLS IN THE AREA
-            signalCells(coord, si, sj);
+            signalCells(p->areas[coord], si, sj, p->gid);
         }
     }
     
-
     hw2_notify(GATHERER_EXITED, p->gid, 0, 0);
 
     return NULL;
@@ -322,6 +309,7 @@ void *gatherer(void *arg){ //arguments: grid, private
 void *commander(void* arg){
     commanderInput* inp = (commanderInput*) arg;
     executeOrders(*(inp->orders),inp->tids);
+    return NULL;
 }
 
 
@@ -335,16 +323,19 @@ int main(){
     numOfGatherers = new int*[n];
     gridMutex = new pthread_mutex_t*[n];
     gridSem = new sem_t*[n];
+    cvAvailable = new pthread_cond_t**[n];
     for(int i=0;i<n;i++){ // Cigbutt counts
         grid[i] = new int[m];
         numOfGatherers[i] = new int[m];
         gridMutex[i] = new pthread_mutex_t[m];
         gridSem[i] = new sem_t[m];
+        cvAvailable[i] = new pthread_cond_t*[m];
         for(int j=0;j<m;j++){
             cin >> grid[i][j];
             numOfGatherers[i][j] = 0;
             pthread_mutex_init(&gridMutex[i][j], NULL); 
             sem_init(&gridSem[i][j],0,1); 
+            cvAvailable[i][j] = NULL;
         }
     }
     
@@ -377,6 +368,9 @@ int main(){
         cin >> ms >> command;
         orders.push_back(make_pair(ms,command));
     }
+
+    Break = 0;
+    Stop = 0;
 
     pthread_t tids[numberOfPrivates];
     for(int t=0;t<numberOfPrivates;t++){
